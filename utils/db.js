598e217c5aea4a4df45297cf802e067521f16a54
Fahrizal Shofyan Aziz <fahrizalshofyanaziz@gmail.com>
2023-11-13 00:01:01 +0700

    revisi getMonth()

--- utils/db.js
+++ utils/db.js
@@ -4,9 +4,11 @@ import "dotenv/config";
 mongoose.set("strict", false);
 
 // process.env.MONGO_URI
+// "mongodb+srv://fahrizalshofyanaziz:5bVGkFYXhgViUIO3@cluster0.ezutszz.mongodb.net/rag"
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("mongodb+srv://fahrizalshofyanaziz:5bVGkFYXhgViUIO3@cluster0.ezutszz.mongodb.net/rag"
+  , { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Koneksi MongoDB sukses");
   })
